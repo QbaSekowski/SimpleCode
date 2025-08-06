ed203b3cb9408ffcd0aee2a633534ea912c959f1
Jakub Sekowski <sekowskijakub@gmail.com>
2025-08-06 09:28:41 +0200

    Remove first and last character.

---
+++ src/test/java/mate/academy/RemoveCharsTest.java
@@ -0,0 +1,17 @@
+package mate.academy;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class RemoveCharsTest {
+
+    @Test
+    public void testRemoval() {
+        assertEquals("loquen", RemoveChars.remove("eloquent"));
+        assertEquals("ountr", RemoveChars.remove("country"));
+        assertEquals("erso", RemoveChars.remove("person"));
+        assertEquals("lac", RemoveChars.remove("place"));
+        assertEquals("", RemoveChars.remove("ok"));
+    }
+}
