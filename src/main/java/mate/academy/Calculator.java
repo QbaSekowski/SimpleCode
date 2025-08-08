e80b40f99f5549620b0372f6fe4856496b4e1ff7
Jakub Sekowski <sekowskijakub@gmail.com>
2025-08-08 09:23:18 +0200

    calculator done

---
+++ src/main/java/mate/academy/Calculator.java
@@ -0,0 +1,18 @@
+package mate.academy;
+
+public class Calculator {
+    public static double calculate(double a, double b, String op) {
+        switch (op) {
+            case "+":
+                return a + b;
+            case "-":
+                return a - b;
+            case "*":
+                return a * b;
+            case "/":
+                return a / b;
+            default:
+                throw new IllegalArgumentException("Invalid operator: " + op);
+        }
+    }
+}
