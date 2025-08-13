a58ec492a5e4ff1b4ee1b70802aac4b7bffd5b6a
Jakub Sekowski <sekowskijakub@gmail.com>
2025-08-13 09:34:32 +0200

    SimpleLetterRemoval done

---
+++ src/test/java/mate/academy/SimpleLetterRemovalTest.java
@@ -0,0 +1,18 @@
+package mate.academy;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class SimpleLetterRemovalTest {
+    @Test
+    public void basicTests() {
+        doTest("abracadabra",  1, "bracadabra");
+        doTest("abracadabra",  2, "brcadabra");
+        doTest("abracadabra",  6, "rcdbr");
+        doTest("abracadabra",  8, "rdr");
+        doTest("abracadabra", 50, "");
+    }
+    private void doTest(String s, int k, String expected) {
+        assertEquals(expected, SimpleLetterRemoval.solve(s, k));
+    }
+}
