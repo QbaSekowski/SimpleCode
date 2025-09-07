0b062308f2b271499dff1ed9abf02af6aee95ebd
Jakub Sekowski <sekowskijakub@gmail.com>
2025-09-07 17:47:10 +0200

    isPrime fixed

--- src/main/java/mate/academy/Prime.java
+++ src/main/java/mate/academy/Prime.java
@@ -2,10 +2,11 @@ package mate.academy;
 
 public class Prime {
     public static boolean isPrime(int num) {
-        if (num < 2) {
-            return false;
-        }
-        for (int i = 2; i < num; i++) {
+        if (num < 2) return false;
+        if (num == 2) return true;
+        if (num % 2 == 0) return false;
+        int sqrt = (int)Math.sqrt(num);
+        for (int i = 3; i <= sqrt; i += 2) {
             if (num % i == 0) {
                 return false;
             }
