270267c227248af4d222b6127f10331f52fe430f
Jakub Sekowski <sekowskijakub@gmail.com>
2025-09-18 10:52:18 +0200

    findCubicNumbers - extracting 3 digits numbers done

--- src/main/java/mate/academy/CubicNumbers.java
+++ src/main/java/mate/academy/CubicNumbers.java
@@ -1,8 +1,16 @@
 package mate.academy;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CubicNumbers {
     public static String findCubicNumbers(String string) {
-
+        List<Integer> numbers = new ArrayList<>();
+        for (int i = 0; i < string.length(); i += 3) {
+            int end = Math.min(i + 3, string.length());
+            String part = string.substring(i, end);
+            numbers.add(Integer.parseInt(part));
+        }
 
     }
 }
