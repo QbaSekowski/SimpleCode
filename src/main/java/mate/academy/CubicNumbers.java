badf6f8637bffa10c8a4f151a4836e149d78a5f1
Jakub Sekowski <sekowskijakub@gmail.com>
2025-09-18 11:15:14 +0200

    findCubicNumbers - progress

--- src/main/java/mate/academy/CubicNumbers.java
+++ src/main/java/mate/academy/CubicNumbers.java
@@ -10,25 +10,39 @@ public class CubicNumbers {
             return "Unlucky";
         }
         List<Integer> numbers = new ArrayList<>();
-        for (int i = 0; i < string.length(); i += 3) {
-            int end = Math.min(i + 3, string.length());
-            String number = string.substring(i, end);
-            numbers.add(Integer.parseInt(number));
+        for (int i = 0; i < digitsOnly.length(); i += 3) {
+            int end = Math.min(i + 3, digitsOnly.length());
+            String part = digitsOnly.substring(i, end);
+            numbers.add(Integer.parseInt(part));
         }
-        int[] numbersArray = numbers.stream().mapToInt(Integer::intValue).toArray();
-        for (int i = 0; i < numbersArray.length; i++) {
-            if (isCubic(numbersArray)) {
-                StringBuilder result = new StringBuilder();
-                for (int j = 0; j < numbersArray.length; j++) {
-                    result.append(numbersArray[j]);
-                }
-                return result.toString() + " Lucky";
+        List<Integer> cubicNumbers = new ArrayList<>();
+        int sum = 0;
+        for (int num : numbers) {
+            if (isCubic(num)) {
+                cubicNumbers.add(num);
+                sum += num;
             }
+        }
+        if (cubicNumbers.isEmpty()) {
             return "Unlucky";
+        } else {
+            StringBuilder sb = new StringBuilder();
+            for (int n : cubicNumbers) {
+                sb.append(n).append(" ");
+            }
+            sb.append(sum).append(" Lucky");
+            return sb.toString();
         }
     }
 
-    private static boolean isCubic(int[] numbersArray) {
-
+    private static boolean isCubic(int num) {
+        int original = num;
+        int sum = 0;
+        String s = String.valueOf(num);
+        for (char c : s.toCharArray()) {
+            int d = c - '0';
+            sum += d * d * d;
+        }
+        return sum == original;
     }
 }
