8de972ccda95f2fbd869b43e9766dfd4a7d9ca2e
Jakub Sekowski <sekowskijakub@gmail.com>
2025-10-12 13:54:20 +0200

    position done

---
+++ src/main/java/mate/academy/Kata.java
@@ -0,0 +1,8 @@
+package mate.academy;
+
+public class Kata {
+    public static String position(char alphabet) {
+        int position = alphabet - 'a' + 1;
+        return "Position of alphabet: " + position;
+    }
+}
