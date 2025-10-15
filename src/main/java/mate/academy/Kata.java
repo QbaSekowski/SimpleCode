8251bd4dc1951927478ee498c60854abba1b8d17
Jakub Sekowski <sekowskijakub@gmail.com>
2025-10-15 11:05:34 +0200

    liters done

--- src/main/java/mate/academy/Kata.java
+++
@@ -1,8 +0,0 @@
-package mate.academy;
-
-public class Kata {
-    public static String position(char alphabet) {
-        int position = alphabet - 'a' + 1;
-        return "Position of alphabet: " + position;
-    }
-}
---
+++ src/main/java/mate/academy/KeepHydrated.java
@@ -0,0 +1,7 @@
+package mate.academy;
+
+public class KeepHydrated  {
+    public static int liters(double time)  {
+        return (int) (time * 0.5);
+    }
+}
