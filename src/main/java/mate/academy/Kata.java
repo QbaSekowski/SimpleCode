c2db110a1c6206b98ff3f70095b7583a5970c638
Jakub Sekowski <sekowskijakub@gmail.com>
2025-10-30 11:08:01 +0100

    multiTable done

--- src/main/java/mate/academy/Kata.java
+++ src/main/java/mate/academy/Kata.java
@@ -1,9 +1,11 @@
 package mate.academy;
 
-public class Kata
-{
-    public static int opposite(int number)
-    {
-        return -number;
+class Kata {
+    public static String multiTable(int num) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <=10; i++) {
+            sb.append(i).append(" * ").append(num).append(" = ").append(i * num).append("\n");
+        }
+        return sb.toString().trim();
     }
 }
