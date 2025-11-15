b90e918fff2c77d0a7d20e95c891d379c67f91ac
Jakub Sekowski <sekowskijakub@gmail.com>
2025-11-15 11:26:28 +0100

    roundTo2DecimalPlaces done

---
+++ src/main/java/mate/academy/RoundAndRound.java
@@ -0,0 +1,12 @@
+package mate.academy;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class RoundAndRound {
+    public static double roundTo2DecimalPlaces(double number) {
+        return new BigDecimal(Double.toString(number))
+                .setScale(2, RoundingMode.HALF_UP)
+                .doubleValue();
+    }
+}
