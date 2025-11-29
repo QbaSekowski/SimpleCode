8068da5826da3c5bb43649ecd54dd90cd7476af9
Jakub Sekowski <sekowskijakub@gmail.com>
2025-11-29 14:03:55 +0100

    rps done

--- src/main/java/mate/academy/Kata.java
+++ src/main/java/mate/academy/Kata.java
@@ -1,17 +1,13 @@
 package mate.academy;
 
+import java.util.Objects;
+
 public class Kata {
-    public static String highAndLow(String numbers) {
-        int[] arr = new int[numbers.split(" ").length];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = Integer.parseInt(numbers.split(" ")[i]);
-        }
-        int max = arr[0];
-        int min = arr[0];
-        for (int i = 1; i < arr.length; i++) {
-            if (arr[i] > max) max = arr[i];
-            if (arr[i] < min) min = arr[i];
-        }
-        return max + " " + min;
+    public static String rps(String p1, String p2) {
+        if (Objects.equals(p1, p2)) return "Draw!";
+        if (p1.equals("rock") && p2.equals("scissors")) return "Player 1 won!";
+        if (p1.equals("paper") && p2.equals("rock")) return "Player 1 won!";
+        if (p1.equals("scissors") && p2.equals("paper")) return "Player 1 won!";
+        return "Player 2 won!";
     }
 }
--- src/test/java/mate/academy/KataTest.java
+++ src/test/java/mate/academy/KataTest.java
@@ -1,17 +1,30 @@
 package mate.academy;
 
+import static org.junit.Assert.*;
 import org.junit.Test;
 
-import static junit.framework.TestCase.fail;
-import static org.junit.Assert.*;
-
 public class KataTest {
+
     @Test
     public void test1() {
-        assertEquals("42 -9", Kata.highAndLow("8 3 -5 42 -1 0 0 -9 4 7 4 -4"));
+        System.out.println("Fixed Tests Player 1 won!");
+        assertEquals("Player 1 won!", Kata.rps("rock", "scissors"));
+        assertEquals("Player 1 won!", Kata.rps("scissors", "paper"));
+        assertEquals("Player 1 won!", Kata.rps("paper", "rock"));
     }
     @Test
     public void test2() {
-        assertEquals("3 1", Kata.highAndLow("1 2 3"));
+        System.out.println("Fixed Tests Player 2 won!");
+        assertEquals("Player 2 won!", Kata.rps("scissors", "rock"));
+        assertEquals("Player 2 won!", Kata.rps("paper", "scissors"));
+        assertEquals("Player 2 won!", Kata.rps("rock", "paper"));
     }
+    @Test
+    public void test3() {
+        System.out.println("Fixed Tests Draw!");
+        assertEquals("Draw!", Kata.rps("scissors", "scissors"));
+        assertEquals("Draw!", Kata.rps("paper", "paper"));
+        assertEquals("Draw!", Kata.rps("rock", "rock"));
+    }
+
 }
