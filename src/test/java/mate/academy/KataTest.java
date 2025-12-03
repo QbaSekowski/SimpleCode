13f748d9953b7b77f3e0acef43f43b4ee177cde9
Jakub Sekowski <sekowskijakub@gmail.com>
2025-12-03 12:57:18 +0100

    isAnagram done

---
+++ src/test/java/mate/academy/KataTest.java
@@ -0,0 +1,27 @@
+package mate.academy;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class KataTest {
+    private static void doTest(String a, String b, boolean expected) {
+        String message = String.format("a = \"%s\"\nb = \"%s\"\n", a, b);
+        boolean actual = Kata.isAnagram(a, b);
+        assertEquals(expected, actual, message);
+    }
+
+    @Test
+    void sampleTests() {
+        doTest("", "", true);
+        doTest("ab", "cdcd", false);
+        doTest("ac", "bb", false);
+        doTest("aab", "abb", false);
+        doTest("a", "aaa", false);
+        doTest("foefet", "toffee", true);
+        doTest("Buckethead", "DeathCubeK", true);
+        doTest("Twoo", "Woot", true);
+        doTest("dumble", "bumble", false);
+        doTest("ound", "round", false);
+        doTest("apple", "pale", false);
+    }
+}
