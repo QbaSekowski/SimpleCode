2ab7ec4b8fe68a475a0099a13c69f920fa805234
Jakub Sekowski <sekowskijakub@gmail.com>
2025-12-22 08:33:07 +0100

    grow done

---
+++ src/test/java/mate/academy/KataTest.java
@@ -0,0 +1,17 @@
+package mate.academy;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import org.junit.runners.JUnit4;
+import java.util.*;
+
+// TODO: Replace examples and use TDD development by writing your own tests
+
+public class KataTest {
+    @Test
+    public void testSomething() {
+        assertEquals(6, Kata.grow(new int[]{1,2,3}));
+        assertEquals(16, Kata.grow(new int[]{4,1,1,1,4}));
+        assertEquals(64, Kata.grow(new int[]{2,2,2,2,2,2}));
+    }
+}
