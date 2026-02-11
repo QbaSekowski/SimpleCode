52959f27237b0c8c2e4923c1557089a3a02afe93
Jakub Sekowski <sekowskijakub@gmail.com>
2026-02-11 10:04:38 +0100

    grabscrab done

--- src/test/java/mate/academy/KataTest.java
+++ src/test/java/mate/academy/KataTest.java
@@ -1,12 +1,31 @@
 package mate.academy;
 
-import org.junit.Test;
-import static org.junit.Assert.assertArrayEquals;
-import org.junit.runners.JUnit4;
+import java.util.*;
+import java.util.stream.*;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class KataTest {
-    @Test
-    public void tests() {
-        assertArrayEquals(new int[] {5, 10, 15}, Kata.multiples(3, 5));
+
+class KataTest {
+
+    static private String[][][] DATA = {
+            //    anagram       words_as_array                       expected_as_array
+            {{"trisf"},     {"first"},                             {"first"}},
+            {{"oob"},       {"bob", "baobab"},                     {}},
+            {{"ainstuomn"}, {"mountains", "hills", "mesa"},        {"mountains"}},
+            {{"oolp"},      {"donkey", "pool", "horse", "loop"},   {"pool", "loop"}},
+            {{"ortsp"},     {"sport", "parrot", "ports", "matey"}, {"sport", "ports"}},
+            {{"ourf"},      {"one","two","three"},                 {}}
+    };
+
+    @Test void fixed_tests() {
+        for(String[][] data: DATA){
+            var anagram = data[0][0];
+            var words   = Arrays.stream(data[1]).collect(Collectors.toList());
+            var exp     = Arrays.stream(data[2]).collect(Collectors.toList());
+            var msg     = String.format("Testing \"%s\" against %s", anagram, words);
+            var actual  = Grab.grabscrab(anagram,words);
+            assertEquals(exp, actual, msg);
+        }
     }
 }
