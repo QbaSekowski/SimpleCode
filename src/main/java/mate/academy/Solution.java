af5ad8ba0a7d7578510a412f1330295210d10936
Jakub Sekowski <sekowskijakub@gmail.com>
2026-02-17 10:56:36 +0100

    solve time updated

--- src/main/java/mate/academy/Solution.java
+++ src/main/java/mate/academy/Solution.java
@@ -1,6 +1,10 @@
 package mate.academy;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 class Solution {
     public static String solve(String[] arr) {
