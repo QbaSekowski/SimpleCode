e46f98cc1ff5286bb6194a20ff8094c004944994
Jakub Sekowski <sekowskijakub@gmail.com>
2026-02-22 11:23:16 +0100

    trouble in progress

--- src/main/java/mate/academy/Solution.java
+++ src/main/java/mate/academy/Solution.java
@@ -1,18 +1,22 @@
 package mate.academy;
 
-import java.util.List;
-
 public class Solution {
+    public static int[] trouble(int[] x, int t) {
+        for (int i = 0; i < x.length; i++) {
+            for (int j = 0; j < x.length; j++) {
+                if (i != j && x[i] + x[j] == t) {
+                    int[] result = new int[x.length - 1];
+                    int index = 0;
 
-    public static <T> T last(final List<T> list) {
-        return list.get(list.size() - 1);
-    }
-
-    public static char last(final String string) {
-        return string.charAt(string.length() - 1);
-    }
-
-    public static <T> T last(final T... list) {
-        return list[list.length - 1];
+                    for (int k = 0; k < x.length; k++) {
+                        if (k != j) {
+                            result[index++] = x[k];
+                        }
+                    }
+                    return result;
+                }
+            }
+        }
+        return x;
     }
 }
--- src/test/java/mate/academy/SolutionTest.java
+++ src/test/java/mate/academy/SolutionTest.java
@@ -1,28 +1,14 @@
 package mate.academy;
 
-import org.junit.Test;
-import java.util.Arrays;
-import java.util.List;
-import static org.junit.Assert.assertEquals;
+import org.junit.jupiter.api.Test;
+import static org.junit.Assert.assertArrayEquals;
 
-public class SolutionTest {
-    @Test public void testList() {
-        final List<Integer> list = Arrays.asList(1, 2 ,3 ,4 ,5);
-        final int last = Solution.last(list);
-        assertEquals(5, last);
-    }
-    @Test public void testString() {
-        final String string = "abcde";
-        final char last = Solution.last(string);
-        assertEquals('e', last);
-    }
-    @Test public void testArgs() {
-        final int last = Solution.last(1, 2 ,3 ,4 ,5);
-        assertEquals(5, last);
-    }
-    @Test public void testArray() {
-        final Integer[] array = {1, 2 ,3 ,4 ,5};
-        final int last = Solution.last(array);
-        assertEquals(5, last);
+class SolutionTest {
+    @Test
+    void sampleTest() {
+        assertArrayEquals(new int[]{1, 3, 5, 6, 7, 4}, Solution.trouble(new int[]{1, 3, 5, 6, 7, 4, 3}, 7));
+        assertArrayEquals(new int[]{4, 1, 4}, Solution.trouble(new int[]{4, 1, 1, 1, 4}, 2));
+        assertArrayEquals(new int[]{2, 2}, Solution.trouble(new int[]{2, 6, 2}, 8));
+        assertArrayEquals(new int[]{2}, Solution.trouble(new int[]{2, 2, 2, 2, 2, 2}, 4));
     }
 }
