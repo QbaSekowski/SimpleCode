c4985c44633c28100d0a7d75ad6faad229d85516
Jakub Sekowski <sekowskijakub@gmail.com>
2026-07-17 08:52:12 +0200

    find capitals done

--- src/main/java/mate/academy/Kata.java
+++ src/main/java/mate/academy/Kata.java
@@ -1,11 +1,16 @@
 package mate.academy;
 
-public class Kata {
+import java.util.ArrayList;
+import java.util.List;
 
-    public static String solution(String str) {
-        StringBuilder sb = new StringBuilder(str);
-        sb.reverse();
-        return sb.toString();
+public class Kata{
+    public static int[] capitals(String s){
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (Character.isUpperCase(s.charAt(i))) {
+                result.add(i);
+            }
+        }
+        return result.stream().mapToInt(i -> i).toArray();
     }
-
 }
